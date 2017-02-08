1f746607d2e574f596b9bcba5dde7d51a97e1ad9
anton <antoneklund1@hotmail.com>
2017-02-08 22:01:35 +0100

    js klart

--- webpage/js/script.js
+++ webpage/js/script.js
@@ -13,35 +13,92 @@ function Item(name, kcal, gluten, lactose, src){
     this.infoKcal = function(){
         return this.itemKcal;
     }
-    this.src = function(){
+    this.source = function(){
         return this.imageSource;
+    }
+    this.infoGluten = function(){
+        if (this.contGluten){
+            return "Gluten"
+        }else{
+            return "Glutenfri"
+        }
+    }
+    this.infoLactose = function(){
+        if (this.contLactose){
+            return "Laktos"
+        }else{
+            return "Laktosfri"
+        }
     };
+    
 }
 
 function printBurgers(){
     var a = new Item("Originalburgare",1337, true, false, "pics/Burgers-Max-Original.jpg");
     var b = new Item("Cheese'n'Bacon",2000, true, true, "pics/Burgers-GDL-Cheese-n-bacon.jpg");	
     var c = new Item("Hallomiburgare",400, false, false, "pics/Burgers-GDL-BBQ-Halloumi-2017.jpg");
-    var arr = [a,b,c];
+    var burgers = [a,b,c];
     var name = document.getElementById("name");
-    var imageRow = document.getElementById("image");
+    var imageRow = document.getElementById("images");
+    var infoRow = document.getElementById("info");
+    var boxesRow = document.getElementById("boxes");
     
     name.setAttribute("align", "left");
     var i = 0;
-    for (i = 0; i<arr.length; i++){
+    for (i = 0; i<burgers.length; i++){
         var th = document.createElement("th");
-        var txt = document.createTextNode(arr[i].infoName());
+        var txt = document.createTextNode(burgers[i].infoName());
         th.appendChild(txt);
         name.appendChild(th);
 
-        var imageColumn = document.createNewElement("td");
-        var image = document.createNewElement("img");
-        image.setAttribute("src", arr[i].imageSource());
-        image.setAttribute("title", arr[i].infoName());
-        image.setAttribute("width", 250);
+        var imageColumn = document.createElement("td");
+        var image = document.createElement("img");
+        image.setAttribute("src", burgers[i].source());
+        //image.setAttribute("title", burgers[i].infoName());
+        image.setAttribute("width", "250");
         imageColumn.appendChild(image);
         imageRow.appendChild(imageColumn);
 
+        var infoColumn = document.createElement("td");
+        var dl = document.createElement("dl");
+        var dt = document.createElement("dt");
+        var dd = document.createElement("dd");
+        var ul = document.createElement("ul");
+        var section = document.createElement("section");
+        var liKcal = document.createElement("li");
+        var liGlut = document.createElement("li");
+        var liLact = document.createElement("li");
+        var iKcal = document.createTextNode(burgers[i].infoKcal());
+        var iGlut = document.createTextNode(burgers[i].infoGluten());
+        var iLact = document.createTextNode(burgers[i].infoLactose());
+        var inneh = document.createTextNode("Innehåll: ");
+        section.setAttribute("class", "ingredients");
+        liKcal.appendChild(iKcal);
+        liGlut.appendChild(iGlut);
+        liLact.appendChild(iLact);
+        section.appendChild(liKcal);
+        section.appendChild(liGlut);
+        section.appendChild(liLact);
+        ul.appendChild(section);
+        dd.appendChild(ul);
+        dt.appendChild(inneh);
+        dt.appendChild(dd);
+        dl.appendChild(dt);
+        infoColumn.appendChild(dl);
+        infoRow.appendChild(infoColumn);
+
+        var boxesColumn = document.createElement("td");
+        var input = document.createElement("input");
+        var boxText = document.createTextNode("Klicka för att välja");
+        input.setAttribute("type", "checkbox");
+        input.setAttribute("name", "choices");
+        input.setAttribute("value", burgers[i].infoName());
+        boxesColumn.appendChild(input);
+        boxesColumn.appendChild(boxText);
+        boxesRow.appendChild(boxesColumn);
+        
+        
+        
     }
 
 
@@ -59,3 +116,31 @@ function indexPageLoaded(){
 }
 
 
+function gatherOrders(){
+    //var orderButton = document.getElementById("order");
+    var sodas = document.getElementById("val");
+    var sodaOption = sodas.options[sodas.selectedIndex].text;
+    var coffeeChoices = document.getElementsByName("option");
+    var coffeeOption;
+    var burgerChoices = document.getElementsByName("choices");
+    var burgerOption = [];
+    for (var i = 0; i<coffeeChoices.length; i++){
+        if (coffeeChoices[i].checked){
+            coffeeOption = coffeeChoices[i].value;
+        }
+    }
+    for (var i = 0; i<burgerChoices.length; i++){
+        if (burgerChoices[i].checked){
+        //    burgerOption[i] = burgerChoices[i].value;
+            console.log(burgerChoices[i].value);
+        }
+    }
+
+    console.log(sodaOption);
+    console.log(coffeeOption);
+}
+
+function orderTrigger(){
+    var orderButton = document.getElementById("order");
+    orderButton.addEventListener("click", gatherOrders());
+}
